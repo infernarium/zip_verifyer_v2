38490c4d6e57217994008b2982a0511595e465a5
Mark <mark.tumanyan@yandex.ru>
2025-07-08 18:42:45 +0300

    add task repo implementation

--- app/infrastructure/repositories/task_repository_impl.py
+++ app/infrastructure/repositories/task_repository_impl.py
@@ -1,12 +1,21 @@
 from sqlalchemy.ext.asyncio import AsyncSession
+from app.domain.models.task import TaskResult
 from app.domain.repositories.task_repository import TaskRepository
-from app.domain.schemas.task import TestResults
+from app.domain.schemas.task import UploadResponseSchema
 
 
 class TaskRepositoryImpl(TaskRepository):
     def __init__(self, db: AsyncSession):
         self.db = db
 
-    async def create_task(self, task: TestResults) -> str: ...
+    async def create_task(self, task: TaskResult) -> UploadResponseSchema:
+        self.db.add(task)
+        await self.db.commit()
+        await self.db.refresh(task)
+        return task
 
-    async def task_exists(self, task_id: str) -> bool: ...
+    async def task_exists(self, task_id: str) -> bool:
+        if await self.db.get(TaskResult, task_id):
+            return True
+        else:
+            return False
