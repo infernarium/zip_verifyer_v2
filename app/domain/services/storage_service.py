7d1acf90ccbf891c4530efdf0f80cb8838d1b128
Mark <mark.tumanyan@yandex.ru>
2025-07-11 21:12:38 +0300

    fix abstract storage

--- app/domain/services/storage_service.py
+++ app/domain/services/storage_service.py
@@ -3,13 +3,17 @@ from abc import ABC, abstractmethod
 
 class StorageService(ABC):
     @abstractmethod
-    async def upload_file(self, file_data: bytes, file_hash: str) -> bool:
+    async def upload_file(self, file_data: bytes, file_id: str) -> bool:
         raise NotImplementedError()
 
     @abstractmethod
-    async def file_exists(self, file_hash: str) -> bool:
+    async def file_exists(self, file_id: str) -> bool:
         raise NotImplementedError()
 
     @abstractmethod
-    async def delete_file(self, file_hash: str) -> bool:
+    async def delete_file(self, file_id: str) -> bool:
+        raise NotImplementedError()
+
+    @abstractmethod
+    async def get_file(self, file_id: str) -> bytes:
         raise NotImplementedError()
