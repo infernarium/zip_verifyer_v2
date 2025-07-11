f5320e574c6ebb7981de48ef3643bd896cadfe46
Mark <mark.tumanyan@yandex.ru>
2025-07-11 21:12:25 +0300

    fix abstract analytics

--- app/domain/services/analytics_service.py
+++ app/domain/services/analytics_service.py
@@ -3,4 +3,5 @@ from abc import ABC, abstractmethod
 
 class AnalyticsService(ABC):
     @abstractmethod
-    async def get_stats(self, file_data: bytes) -> bool: ...
+    async def get_stats(self, file: bytes) -> dict:
+        raise NotImplementedError()
