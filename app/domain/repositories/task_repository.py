0d588290ce6fbee7aa6b2060d59ba06a428efda9
Mark <mark.tumanyan@yandex.ru>
2025-07-11 20:42:27 +0300

    fix abstract task repo

--- app/domain/repositories/task_repository.py
+++ app/domain/repositories/task_repository.py
@@ -1,13 +1,17 @@
 from abc import ABC, abstractmethod
-import uuid
-from app.domain.schemas.task import TestResults
+from app.domain.models.task import TaskResult
+from app.domain.schemas.task import TaskUpdateSchema, TestResults, UploadResponseSchema
 
 
 class TaskRepository(ABC):
     @abstractmethod
-    async def create_task(self, task: TestResults) -> str:
+    async def create_task(self, task: TaskResult) -> UploadResponseSchema:
         raise NotImplementedError()
 
     @abstractmethod
     async def task_exists(self, task_id: str) -> bool:
+        raise NotImplementedError()
+
+    @abstractmethod
+    async def update_task(self, task_id: str, update_data: TaskUpdateSchema) -> bool:
         raise NotImplementedError()
