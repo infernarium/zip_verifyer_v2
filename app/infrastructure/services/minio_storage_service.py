340994470b0abc80d5a4bffae162d55945edcf86
Mark <mark.tumanyan@yandex.ru>
2025-07-11 21:14:19 +0300

    fix minio service

--- app/infrastructure/services/minio_storage_service.py
+++ app/infrastructure/services/minio_storage_service.py
@@ -14,8 +14,7 @@ class MinioStorageService(StorageService):
             if not self.client.bucket_exists(settings.MINIO_BUCKET_NAME):
                 self.client.make_bucket(settings.MINIO_BUCKET_NAME)
         except S3Error as e:
-            print(f"ERROR: {e}")
-            exit(1)
+            raise e
 
     async def upload_file(self, file_data: bytes, file_id: str) -> bool:
         file_stream = BytesIO(file_data)
@@ -44,5 +43,13 @@ class MinioStorageService(StorageService):
         try:
             self.client.remove_object(self.settings.MINIO_BUCKET_NAME, file_id)
             return True
+        except S3Error as e:
+            raise e
+
+    async def get_file(self, file_id: str) -> bytes:
+        try:
+            file = self.client.get_object(self.settings.MINIO_BUCKET_NAME, file_id)
+            file_data = file.read()
+            return file_data
         except S3Error as e:
             raise e
