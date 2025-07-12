305aa31da7ace6ce5e084fc5ff654eace827e650
Mark <mark.tumanyan@yandex.ru>
2025-07-12 15:21:15 +0300

    QOL

--- app/main.py
+++ app/main.py
@@ -1,8 +1,12 @@
 #!app/main.py
 from fastapi import FastAPI
+import uvicorn
 from app.api.v1.routers.task import task_router
 
 
 app = FastAPI()
 
 app.include_router(router=task_router)
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
